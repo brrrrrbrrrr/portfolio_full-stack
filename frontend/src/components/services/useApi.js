52e9e5d702f8b61dbcbca6adc5c09f8b8b1f11be
CHAILLAN Benjamin <benjamin.chaillan-pro@laposte.net>
2023-11-16 17:44:41 +0100

    ignore env and put variable in useapi

--- frontend/src/components/services/useApi.js
+++ frontend/src/components/services/useApi.js
@@ -3,10 +3,13 @@ import axios from "axios";
 
 let apiSingleton = null;
 
+const apiUrl = "http://srv441095.hstgr.cloud/api";
+
 const useApi = () => {
   if (!apiSingleton) {
     apiSingleton = axios.create({
-      baseURL: import.meta.env.VITE_APP_API_URL,
+      // baseURL: import.meta.env.VITE_APP_API_URL,
+      baseURL: apiUrl,
     });
   }
   return apiSingleton;
